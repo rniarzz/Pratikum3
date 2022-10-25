912aae7c7ea630462ade4354b7fd5f1109f2cb6f
rniarzz <115542704+rniarzz@users.noreply.github.com>
2022-10-25 23:22:51 +0700

    Add files via upload

---
+++ lingkaran.py
@@ -0,0 +1,7 @@
+print('Menghitung luas lingakaran')
+print('==============================')
+r = int(input('masukan jari-jari lingkaran: '))
+phi = 3.14
+L = phi * (r * r)
+print('Luas lingakaran dengan jari-jari {} adalah {}'.format(r, L))
+
