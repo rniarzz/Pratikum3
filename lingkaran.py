0c3e9a345b4b628e4cf069c92821fec5232e84ae
rniarzz <115542704+rniarzz@users.noreply.github.com>
2022-10-27 13:57:29 +0700

    Update lingkaran.py

--- lingkaran.py
+++ lingkaran.py
@@ -3,5 +3,7 @@ print('==============================')
 r = int(input('masukan jari-jari lingkaran: '))
 phi = 3.14
 L = phi * (r * r)
+K = 2 * phi * r
 print('Luas lingakaran dengan jari-jari {} adalah {}'.format(r, L))
+print('keliling lingakaran dengan jari-jari {} adalah {}'.format(r, K))
 
